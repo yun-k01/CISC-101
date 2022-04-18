87895900dc2fd973b9310b12b5dbd09959ec69b6
yun-k01 <83739801+yun-k01@users.noreply.github.com>
2022-04-18 18:18:30 -0400

    Add files via upload

---
+++ Assignment 10.py
@@ -0,0 +1,170 @@
+"""
+Assignment 10: Mock Final Exam
+"""
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+import urllib.request
+
+"""
+Question 1
+"""
+
+def readURLData():
+    response = urllib.request.urlopen("https://research.cs.queensu.ca/home/cords2/timHortons.txt")
+    html = response.readline()
+    
+    # dictionaries to hold the orders and the items within the orders
+    orders = {}
+    items = {}
+    
+    try: 
+        while len(html) != 0:
+            data = html.decode('utf-8').split()  
+            
+            orderNos = data[0]   # selecting the order number
+            orderNos = int(orderNos)   # making the order number an integer instead of a string
+            
+            x = data[1:-1]   # selecting the food items
+            y = data[-1]   # selecing the cost 
+            y = float(y)   # making the cost a float instead of a string
+            
+            items = {"food": x, "cost": y}   # assigning the food and cost to the items dictionary
+            
+            orders[orderNos] = items   # assigning the items dictionary to the orders dictionary for an order number
+    
+            html = response.readline()
+    except:
+        print("URL Error")
+        return {}
+    
+    return(orders)
+orderDictionary = readURLData()
+print(orderDictionary)
+
+
+
+"""
+Question 2
+"""
+def orderSum(dictionary):
+    """
+    this function calculates the average cost of an order and which order is cheaper
+    input:
+        dictionary - dictionary (keys: integer, values: string + list, string + float)
+    return:
+        avgCost - float
+        cheaper - string
+    """
+    Costs = []
+
+    # for loop to calculate the average cost of an order
+    for i in range(len(dictionary)):
+        x = 101 + i
+        Costs.append(dictionary[x]["cost"])   # creating a list of the different costs
+        
+    avgCost = sum(Costs) / len(dictionary)   # calculating the average cost
+    
+    cheaper = min(Costs)   # determining which of the orders is the cheapest
+    
+    # for loop to determine which order is the cheapest
+    for i in range(len(dictionary)):
+        x = 101 + i
+        if cheaper == dictionary[x]["cost"]:
+            cheapest = x   # selecting the order number
+          
+    return[avgCost, cheapest]
+    
+
+
+"""
+Question 3
+"""
+def removingCoffeeAndWraps(dictionary):
+    """
+    this function removes wraps and exchanges coffee for tea from orders
+    input:
+        dictionary - dictionary (keys: integer, values: string + list, string + float)
+    returns:
+        dictionary - dictionary (keys: integer, values: string + list, string + float)
+    """
+    for i in range(len(dictionary)):
+        x = 101 + i
+        y = dictionary[x]["food"]
+        
+        # exchanging coffee for tea
+        if "Coffee" in y:
+            y.remove("Coffee")
+            y.insert(0, "Tea")
+        
+        # removing wraps from orders and making the cost $2.3 less
+        if "Wrap" in y:
+            y.remove("Wrap")
+            dictionary[x]["cost"] -= 2.3
+            
+    return dictionary
+
+
+
+"""
+Question 4
+"""
+def main():
+    print("Here is the menu of options:")
+    print("""
+    1. Update orders
+    2. Find average and minimum cost of orders
+    3. Insert a new order
+    4. Exit this program
+    """)
+    orderDictionary = readURLData()
+    choices = input("What would you like to do? ")
+    
+    # loop control variable
+    loopControl = 0
+    while loopControl != 1:
+        
+        # updating orders to remove wraps and exchange coffee for tea
+        if choices == "1":
+            orderDictionary = removingCoffeeAndWraps(orderDictionary)
+            print("These are the orders:", orderDictionary, sep = "\n")
+            choices = input("What would you like to do next? ")
+        
+        elif choices == "2":
+            avgOrder = orderSum(orderDictionary)
+            print("The average order is $", avgOrder[0], sep = "")
+            print("The cheapest order is order #", avgOrder[1], sep = "")
+            choices = input("What would you like to do next? ")
+            
+        elif choices == "3":
+            # adding the order number
+            newOrderNos = list(orderDictionary.keys())[-1]
+            newOrderNos = int(newOrderNos) + 1
+                
+            # adding the items to the order
+            newOrderFoodNos = int(input("How many items were ordered? "))
+            newOrder = {}
+            newOrderFoods = []
+            for i in range(newOrderFoodNos):
+                newFood = input("What was ordered? ")
+                newOrderFoods.append(newFood)
+                  
+            newOrderCost = float(input("How much did it cost? $"))
+
+            newOrder = {"food": newOrderFoods, "cost": newOrderCost}
+            orderDictionary[newOrderNos] = newOrder
+            3
+            
+            print("The orders are:", orderDictionary)
+            print("\n")
+            choices = input("What would you like to do next? ")
+        
+        elif choices == "4": 
+            loopControl = 1
+            print("Exiting the program...")
+            
+        else: 
+            choices = input("Invalid input. What would you like to do? ")
+            
+main()
---
+++ Assignment 11.py
@@ -0,0 +1,209 @@
+"""
+Assignment 11: Card Game
+"""
+import random
+
+def cardFromDeck(deck):
+    """
+    this function selects a random card to be flipped and removes it from the deck
+    input:
+        deck - list of strings
+    return:
+        card - string
+    """
+    card = random.choice(deck)  # selecting a random card from the deck
+    deck.remove(card)  # removing the selected card from the deck
+    
+    return card
+
+
+
+def dealHand(deck):
+    """
+    this function selects 5 cards from a deck to into a list and removes the cards from the deck
+    input:
+        deck - list of strings
+    return:
+        hand - list of strings
+        
+    """
+    hand = []  # storing the selected cards into a list
+    
+    for i in range(5):
+        card = random.choice(deck)  # selecting a random card
+        hand.append(card)  # adding the card to a list
+        deck.remove(card)  # removing the card from the deck
+        
+    return hand
+    
+    
+
+def userTurn(deck, userHand, userChoice, dealCard):
+    """
+    this function will change the hand of the player or will flip a card depending on their choice
+    input:
+        deck - list of strings
+        userHand - list of strings
+        userChoice - string
+        dealCard - string
+    return:
+        userCards - list of strings
+        """
+    # if the user selects to choose a card from the deck, the selected card will be added to their hand and they will remove a card in their hand
+    if userChoice == "D":
+        print("Your hand:", userHand)
+        userHand.append(dealCard)  # adding the card to the user's hand
+        removeCard = input("Which card would you like to remove? " )
+        
+        # ensuring the selected card is valid card
+        while not removeCard in userHand:
+            removeCard = input("Invalid input. Which card would you like to remove? ")
+
+        # removing the selected card and telling the user their new hand of cards   
+        if removeCard in userHand:
+            userHand.remove(removeCard)
+            print("Your hand:", userHand)
+    
+    # if the user selects F, the computer will flip a card during it's turn
+
+    return userHand
+    
+
+
+def computerTurn(deck, compHand, userChoice):
+    """
+    this function gives the computer 5 cards and removes them from the deck
+    input:
+        cards - list of strings
+    return:
+        compCards - list of strings
+    """
+    # if the user selected F, the computer will keep the flipped card and remove a card in their hand
+    if userChoice == "F":
+        dealCard = cardFromDeck(deck)
+        compHand.append(dealCard)
+        print("The flipped card is:", dealCard)
+        removeCard = random.choice(compHand)
+        compHand.remove(removeCard)
+        print("Computer hand:", compHand)
+            
+    # if the user selected D, the computer will flip a card
+    else:
+        flipCard = cardFromDeck(deck)
+        print("Computer hand:", compHand)
+    
+    return compHand
+
+
+
+def checkWin(compCards, userCards):
+    """
+    this function checks which hand of cards wins
+    input:
+        compCards - list of strings
+        userCards - list of strings
+    return:
+        winner - string
+    """
+    # checking sum of user
+    valuesUser = []  # list to store the rank of the cards
+    userSum = 0
+    for i in range(5):
+        valuesUser.append(userCards[i][0])  # storing the rank of each card
+
+    for i in range(5):
+        if valuesUser[i].isalpha() == True:
+            userSum += 10  # if the card is A, K, Q, or J, it has a value of 10
+        else: 
+            userSum += int(valuesUser[i])  # making the card an integer so it can be added to the sum
+    
+    # checking sum of computer
+    valuesComp = []  # list to store the rank of the cards
+    compSum = 0
+    for i in range(5):
+        valuesComp.append(userCards[i][0])  # storing the rank of each card
+
+    for i in range(5):
+        if valuesComp[i].isalpha() == True:
+            compSum += 10  # if the card is A, K, Q, or J, it has a value of 10
+        else: 
+            compSum += int(valuesComp[i])  # making the card an integer so it can be added to the sum
+    
+    # comparing sums
+    winningVal = max(userSum, compSum)
+
+    if winningVal == userSum:
+        winner = "User"
+    elif winningVal == compSum:
+        winner = "Computer"
+    elif userSum == compSum:
+        winner= "Tie"
+    else:
+        winner = "None"
+
+    # checking suits of user
+    for i in range(5):
+        if userCards[i] in userCards[i-1]:
+            winner = userCards
+        
+    # checking suits of computer
+    for i in range(5):
+        if compCards[i] in compCards[i-1]:
+            winner = compCards
+        
+    return winner
+
+
+def main():
+    cards = ["AH", "AD", "AS", "AC", "KH", "KD", "KS", "KC",
+                    "QH", "QD", "QS", "QC", "JH", "JD", "JS", "JC",
+                    "10H", "10D", "10S", "10C", "9H", "9D", "9S", "9C",
+                    "8H", "8D", "8S", "8C", "7H", "7D", "7S", "7C",
+                    "6H", "6D", "6S", "6C", "5H", "5D", "5S", "5C",
+                    "4H", "4D", "4S", "4C", "3H", "3D", "3S", "3C",
+                    "2H", "2D", "2S", "2C"]
+    # starting the game by giving the user their hand and the computer it's hand
+    userHand = dealHand(cards)
+    print("Your hand:", userHand)
+    compHand = dealHand(cards)
+    print("Computer hand:", compHand)
+        
+    # loop control so game is played again if there is no winner
+    loopControl = 0
+    while loopControl != 1:
+
+        # user's turn
+        print("\n")
+        dealCard = cardFromDeck(cards)
+        print("The flipped card is:", dealCard)  # flipping a card to start the game
+        userChoice = input("What would you like to do? D = choose from deck, F = pick up flipped card: ")
+                
+        # making sure the user input is valid
+        while userChoice != "D" and userChoice != "F":
+            userChoice = input("Invalid Input. What would you like to do? D = choose from deck, F = pick up flipped card: ")
+                
+        userHand = userTurn(cards, userHand, userChoice, dealCard)
+                
+        # computer's turn
+        compHand = computerTurn(cards, compHand, userChoice)
+            
+        # checking the winner
+        winner = checkWin(compHand, userHand)
+            
+        # informing the winner and playing again if there is no winner
+        print("\n")
+        if winner == "User":
+            print("Congratulations! You are the winner!")
+            loopControl = 1
+        elif winner == "Computer":
+            print("Sorry! The computer won!")
+            loopControl = 1
+        elif winner == "Tie":
+            print("There was a tie!")
+            loopControl = 1
+        else:
+            print("There is no winner! Play again!")    
+    
+    print("Thank you for playing!")
+
+main()
